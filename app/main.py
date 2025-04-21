08b24e927618548686ea596237fa25c009aa6eaf
Alex Ausch <alex@ausch.name>
2025-04-21 17:30:57 -0400

    Include a dependency example

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
+from termcolor import cprint
+
 from package.hello import say_hi
+
 
 def greet():
     response = say_hi()
     return f"The Python package says, '{response}'"
 
-
-print(greet())
+if __name__ == "__main__":
+   cprint(greet(), "red", attrs=["bold"])
